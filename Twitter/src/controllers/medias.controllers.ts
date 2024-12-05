6ca72d368731499905ec39adf38abb9b7c893fa3
vtrong <trongdn2405@gmail.com>
2024-12-05 19:04:29 +0700

    Filter với key của ảnh upload

--- Twitter/src/controllers/medias.controllers.ts
+++ Twitter/src/controllers/medias.controllers.ts
@@ -2,26 +2,43 @@ import { NextFunction, Response, Request } from "express";
 import path from "path";
 import { initFolder } from "~/utils/file";
 
-const uploadDir = path.resolve('uploads/images')
-initFolder(uploadDir)
+const uploadDir = path.resolve('uploads/images');
+initFolder(uploadDir);
+
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  const formidable = (await import('formidable')).default;
-  const form = formidable({
-    uploadDir: uploadDir,
-    maxFiles: 1,
-    keepExtensions: true,
-    maxFileSize: 300 * 1024,
-  });
+  try {
+    const formidable = (await import('formidable')).default;
 
-  // Xử lý tải tệp
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      return next(err);
-    }
-
-    res.json({
-      message: 'Upload image successfully',
-      files: files
+    const form = formidable({
+      uploadDir: uploadDir,
+      maxFiles: 1,
+      keepExtensions: true,
+      maxFileSize: 300 * 1024,
+      filter: function ({ name, originalFilename, mimetype }) {
+        const valid = name === 'image' && Boolean(mimetype?.includes('image/'))
+        if (!valid) {
+          form.emit('error' as any, new Error('File is not valid') as any)
+        }
+        return valid
+      }
     });
-  });
+
+    // Xử lý tải tệp
+    form.parse(req, (err, fields, files) => {
+      if (err) {
+        return next(err)
+      }
+
+      if (!Boolean(files.image)) {
+        return next(new Error('File is empty'))
+      }
+
+      res.json({
+        message: 'Upload image successfully',
+        files: files.image
+      })
+    })
+  } catch (err) {
+    next(err)
+  }
 };
