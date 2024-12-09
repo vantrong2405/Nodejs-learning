5c9425e9909f2c822c44937c6115f2eaf34c8252
vtrong <trongdn2405@gmail.com>
2024-12-09 17:38:45 +0700

    Add function hls streaming

--- Twitter/src/routes/statics.router.ts
+++ Twitter/src/routes/statics.router.ts
@@ -1,7 +1,9 @@
 import { Router } from 'express'
-import { serveImageController, serveVideoStreamController } from '~/controllers/medias.controllers'
+import { serveImageController, serveM3U8Controller, serveSegmentController, serveVideoStreamController } from '~/controllers/medias.controllers'
 const staticRoutes = Router()
 staticRoutes.get('/image/:name', serveImageController)
 staticRoutes.get('/video-stream/:name', serveVideoStreamController)
+staticRoutes.get('/video-hls/:id/master.m3u8', serveM3U8Controller)
+staticRoutes.get('/video-hls/:id/:v/:segment', serveSegmentController)
 export default staticRoutes
 
